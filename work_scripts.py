f07ca693d69f9d4352f087bd5b47722b6a38bb03
Roman Mamaev <mamaew.roma2000@yandex.ru>
2024-03-01 22:48:20 +0300

    исправил некорректное скачивание статей с infobox-ами

--- work_scripts.py
+++ work_scripts.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 from string_utils import delete_inside_parentheses_and_brackets
 from utils import rate_is_text_about_IT, parallel_map
+import re
 
 
 def step_1():
@@ -22,16 +23,25 @@ def step_1():
 
 def rate_is_link_about_IT(idx, link_dict):
     try:
-        page = requests.get(link_dict['link']).text
-        page_soup = BeautifulSoup(page, 'html.parser')
-        first_p = page_soup.find('div', {'class': 'mw-content-ltr mw-parser-output'}).find('p')
-        definition = delete_inside_parentheses_and_brackets(first_p.text)
+        definition = first_p_by_link(link_dict['link'])
         link_dict['about_IT'] = rate_is_text_about_IT(definition)
         return link_dict
     except:
         link_dict['about_IT'] = None
         print('ERROR ON', link_dict)
         return link_dict
+
+
+def first_p_by_link(link):
+    page = requests.get(link).text
+    page_soup = BeautifulSoup(page, 'html.parser')
+    article = page_soup.find('div', {'class': 'mw-content-ltr mw-parser-output'})
+    infobox_tables = article.find_all('table', {'class': 'infobox'})
+    for table in infobox_tables:
+        table.decompose()
+    first_p = article.find('p', class_=lambda x: x != "mw-empty-elt" and x != 'wikidata-claim')
+    definition = delete_inside_parentheses_and_brackets(first_p.text)
+    return definition
 
 
 def step_2():
@@ -42,5 +52,35 @@ def step_2():
     with open('links-8-with-rates-extended.json', 'w') as file:
         json.dump(r, file, ensure_ascii=False, indent=4)
 
+
+def has_russian_letters(string):
+    russian_letters_pattern = re.compile(r'[а-яА-Я]')
+    return bool(russian_letters_pattern.search(string))
+
+
+def is_not_computer_game(link):
+    text = first_p_by_link(link).lower()
+    print('text', text, 'видеоигр' not in text and not re.search(r'компьютерн.{1,20}игр', text))
+    return 'видеоигр' not in text and not re.search(r'компьютерн.{1,20}игр', text)
+
+
+def step_3():
+    with open('links-8-with-rates-extended.json', 'r') as file:
+        j = json.load(file)
+    j_not_none = [x for x in j if x['about_IT'] is not None]
+    # j_none = [x for x in j if x['about_IT'] is None]
+    sorted_data = sorted(j_not_none, key=lambda x: x['about_IT'], reverse=True)
+    # sorted_data = [x for x in sorted_data[:20] if is_not_computer_game(x['link'])]
+    with open('it-first.json', 'w') as file:
+        json.dump(sorted_data, file, ensure_ascii=False, indent=4)
+
+
+#
+#
+# # step_3()
+#
+# l = 'https://ru.wikipedia.org/wiki/Need_for_Speed:_Undercover'
+# a = first_p_by_link(l).lower()
+# print(a)
 
 step_2()
